b3ea4692c21596f2b1a56c0174fcca6c18a7b961
hbyyy <qjaduddl94@gmail.com>
2019-12-11 14:42:43 +0900

    post_list view에서 복잡한 html문자열을 전송

--- blog/views.py
+++ blog/views.py
@@ -4,4 +4,20 @@ from django.shortcuts import render
 
 # Create your views here.
 def post_list(request):
-    return HttpResponse('<html><body><h1>Post List!</h1><body><html>')
+    return HttpResponse(
+        '<html>'
+        '<body>'
+        '<h1>Post List!</h1>'
+        '<div>'
+        '<p>publish: 12.11.2019, 14:38</p>'
+        '<h2><a href="">My First post</a></h2>'
+        '<p>asdadasdasdasd</p>'
+        '</div>'
+        '<div>'
+        '<p>published: 12.11.2019, 14.39</p>'
+        '<h2><a href=">My second posy</a></h2>'
+        '<p>adadlxxlxlxlxllx</p>'
+        '</div>'
+        '</body>'
+        '</html>'
+    )
